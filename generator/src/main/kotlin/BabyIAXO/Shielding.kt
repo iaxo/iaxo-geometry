f267f87a174fb7893bf54e7764e5ca54b1bc929c
María Jiménez <106647320+mariajmz@users.noreply.github.com>
2024-04-01 13:00:09 +0200

    Adding copperBox to shielding (#5)
    * adding copperBox to shielding
    
    * do not add last shielding layer if no thickness
    
    * gdml
    
    * variation with no copper box
    
    ---------
    
    Co-authored-by: Luis Antonio Obis Aparicio <luis.antonio.obis@gmail.com>

--- generator/src/main/kotlin/BabyIAXO/Shielding.kt
+++ generator/src/main/kotlin/BabyIAXO/Shielding.kt
@@ -4,18 +4,20 @@ import Geometry
 
 import space.kscience.gdml.*
 
-open class Shielding(open val layers: Boolean = false) : Geometry() {
+open class Shielding(open val layers: Boolean = false, open val copperBox: Boolean = true) : Geometry() {
     companion object Parameters {
-        const val SizeXY: Double = 590.0
-        const val SizeZ: Double = 540.0
+        const val SizeXY: Double = 600.0
+        const val SizeZ: Double = 550.0
 
-        const val ShaftShortSideX: Double = 194.0
-        const val ShaftShortSideY: Double = 170.0
-        const val ShaftLongSide: Double = 340.0
+        const val ShaftShortSideX: Double = 200.0
+        const val ShaftShortSideY: Double = 200.0
+        const val ShaftLongSide: Double = 350.0
+
+        const val copperBoxThickness: Double = 10.0
 
         const val MultiLayerThickness: Double = 40.0
 
-        private const val DetectorToShieldingSeparation: Double = -60.0
+        private const val DetectorToShieldingSeparation: Double = -60.0 + copperBoxThickness
         const val EnvelopeThickness: Double = 10.0
         const val OffsetZ: Double =
             DetectorToShieldingSeparation + Chamber.Height / 2 + Chamber.ReadoutKaptonThickness + Chamber.BackplateThickness
@@ -24,6 +26,27 @@ open class Shielding(open val layers: Boolean = false) : Geometry() {
     override fun generate(gdml: Gdml): GdmlRef<GdmlAssembly> {
         if (!layers) {
             val shieldingVolume: GdmlRef<GdmlAssembly> by lazy {
+                val copperBoxOutterSolid =
+                    gdml.solids.box(
+                        ShaftShortSideX,
+                        ShaftShortSideY,
+                        ShaftLongSide,
+                        "copperBoxOutterSolid"
+                    )
+                val copperBoxInnerSolid =
+                    gdml.solids.box(
+                        ShaftShortSideX - 2 * copperBoxThickness,
+                        ShaftShortSideY - 5 * copperBoxThickness,
+                        ShaftLongSide,
+                        "copperBoxInnerSolid"
+                    )
+                val copperBoxSolid =
+                    gdml.solids.subtraction(copperBoxOutterSolid, copperBoxInnerSolid, "copperBoxSolid") {
+                        position(z = copperBoxThickness) { unit = LUnit.MM }
+                    }
+                val copperBoxVolume =
+                    gdml.structure.volume(Materials.Copper.ref, copperBoxSolid, "copperBoxVolume")
+
                 val leadBoxSolid =
                     gdml.solids.box(SizeXY, SizeXY, SizeZ, "leadBoxSolid")
                 val leadBoxShaftSolid =
@@ -45,6 +68,10 @@ open class Shielding(open val layers: Boolean = false) : Geometry() {
                     physVolume(leadShieldingVolume, name = "shielding20cm") {
                         position(z = -OffsetZ) { unit = LUnit.MM }
                     }
+                    physVolume(copperBoxVolume, name = "copperBox") {
+                        position(z = -OffsetZ + SizeZ / 2 - ShaftLongSide / 2) { unit = LUnit.MM }
+                    }
+
                 }
             }
 
@@ -87,34 +114,66 @@ open class Shielding(open val layers: Boolean = false) : Geometry() {
                     shieldingLayerList.add(shieldingLayerBoxWithHoleVolume)
                 }
 
+                var shieldingLayerLastNullable: GdmlRef<GdmlVolume>? = null;
                 // last layer
-                val leadBoxLastLayerSolid =
-                    gdml.solids.box(SizeXY, SizeXY, SizeZ, "leadBoxSolid")
-                val leadBoxLastLayerShaftSolid =
-                    gdml.solids.box(
-                        ShaftShortSideX + 2 * MultiLayerThickness * numberOfLayers,
-                        ShaftShortSideY + 2 * MultiLayerThickness * numberOfLayers,
-                        ShaftLongSide + MultiLayerThickness * numberOfLayers,
-                        "leadBoxShaftSolid"
-                    )
-                val leadBoxWithShaftSolid =
-                    gdml.solids.subtraction(
-                        leadBoxLastLayerSolid,
-                        leadBoxLastLayerShaftSolid,
-                        "leadBoxWithShaftSolid"
-                    ) {
-                        position(z = SizeZ / 2 - (ShaftLongSide + MultiLayerThickness * numberOfLayers) / 2) {
-                            unit = LUnit.MM
+                if (ShaftShortSideX + 2 * MultiLayerThickness * numberOfLayers < SizeXY || ShaftShortSideX + 2 * MultiLayerThickness * numberOfLayers < SizeXY || ShaftLongSide + MultiLayerThickness * numberOfLayers < SizeZ) {
+                    val leadBoxLastLayerSolid =
+                        gdml.solids.box(SizeXY, SizeXY, SizeZ, "leadBoxSolid")
+                    val leadBoxLastLayerShaftSolid =
+                        gdml.solids.box(
+                            ShaftShortSideX + 2 * MultiLayerThickness * numberOfLayers,
+                            ShaftShortSideY + 2 * MultiLayerThickness * numberOfLayers,
+                            ShaftLongSide + MultiLayerThickness * numberOfLayers,
+                            "leadBoxShaftSolid"
+                        )
+                    val leadBoxWithShaftSolid =
+                        gdml.solids.subtraction(
+                            leadBoxLastLayerSolid,
+                            leadBoxLastLayerShaftSolid,
+                            "leadBoxWithShaftSolid"
+                        ) {
+                            position(z = SizeZ / 2 - (ShaftLongSide + MultiLayerThickness * numberOfLayers) / 2) {
+                                unit = LUnit.MM
+                            }
                         }
-                    }
-                val shieldingLayerLast =
-                    gdml.structure.volume(Materials.Lead.ref, leadBoxWithShaftSolid, "shieldingVolumeLayerLast")
+                    shieldingLayerLastNullable =
+                        gdml.structure.volume(Materials.Lead.ref, leadBoxWithShaftSolid, "shieldingVolumeLayerLast")
+                }
 
+                val copperBoxOutterSolid =
+                    gdml.solids.box(
+                        ShaftShortSideX,
+                        ShaftShortSideY,
+                        ShaftLongSide,
+                        "copperBoxOutterSolid"
+                    )
+                val copperBoxInnerSolid =
+                    gdml.solids.box(
+                        ShaftShortSideX - 2 * copperBoxThickness,
+                        ShaftShortSideY - 5 * copperBoxThickness,
+                        ShaftLongSide,
+                        "copperBoxInnerSolid"
+                    )
+                val copperBoxSolid =
+                    gdml.solids.subtraction(copperBoxOutterSolid, copperBoxInnerSolid, "copperBoxSolid") {
+                        position(z = copperBoxThickness) { unit = LUnit.MM }
+                    }
+                val copperBoxVolume =
+                    gdml.structure.volume(Materials.Copper.ref, copperBoxSolid, "copperBoxVolume")
 
                 return@lazy gdml.structure.assembly {
                     name = "shielding"
-                    physVolume(shieldingLayerLast, name = "shieldingLayerLast") {
-                        position(z = -OffsetZ, x = 0) { unit = LUnit.MM }
+
+                    if (copperBox) {
+                        physVolume(copperBoxVolume, name = "copperBox") {
+                            position(z = -OffsetZ + SizeZ / 2 - ShaftLongSide / 2) { unit = LUnit.MM }
+                        }
+                    }
+
+                    if (shieldingLayerLastNullable != null) {
+                        physVolume(shieldingLayerLastNullable, name = "shieldingLayerLast") {
+                            position(z = -OffsetZ, x = 0) { unit = LUnit.MM }
+                        }
                     }
 
                     for ((index, value) in shieldingLayerList.withIndex()) {
