8cbb0622e1055a4b344e8ceb1cf4a4489e89e685
mariajmz <maria.jzpy@gmail.com>
2025-01-15 17:38:54 +0100

    adding option to get a detailed description of the electronics in IAXO-D1

--- generator/src/main/kotlin/IAXO-D1/Electronics.kt
+++ generator/src/main/kotlin/IAXO-D1/Electronics.kt
@@ -4,8 +4,9 @@ import Geometry
 
 import space.kscience.gdml.*
 
-open class Electronics : Geometry() {
-
+open class Electronics (    
+    open val detailedElectronics: Boolean = false,
+) : Geometry() {
     companion object Parameters {
 
         // Box : Dimensions 
@@ -24,6 +25,15 @@ open class Electronics : Geometry() {
         const val flatCableShortSideX: Double = 1.0
         const val flatCableShortSideY: Double = 70.0
 
+        //chip Dimensions
+        const val chipSideYZ: Double = 25.0
+        const val chipShortSideX: Double = 1.0
+
+        //components Dimensions
+        const val componentsLongSideZ: Double = 5.0
+        const val componentsShortSideX: Double = 1.0
+        const val componentsShortSideY: Double = 40.0
+
         // Distance 
         const val DetectorToElectronicsDistanceZ: Double = 235.0
         const val PipeToElectronicsOffSetX: Double = 5.0
@@ -32,8 +42,12 @@ open class Electronics : Geometry() {
     }
 
     override fun generate (gdml : Gdml): GdmlRef<GdmlAssembly> {
-        val electronicsBoxVolume: GdmlRef<GdmlAssembly> by lazy {
 
+        if(!detailedElectronics){
+
+            val electronicsBoxVolume: GdmlRef<GdmlAssembly> by lazy {
+
+            //Box 
             val electronicsBoxOutSolid = gdml.solids.box(
                 electronicsBoxShortSideX,
                 electronicsBoxShortSideY,
@@ -72,7 +86,7 @@ open class Electronics : Geometry() {
             val electronicsBoxFillingVolume =
             gdml.structure.volume(Materials.Vacuum.ref,electronicsBoxInSolid,"electronicsBoxFillingVolume")
 
-             val electronicsCardVolume = 
+            val electronicsCardVolume = 
             gdml.structure.volume(Materials.Copper.ref,electronicsCardSolid,"electronicsCardVolume")
 
             val flatCableVolume =
@@ -81,9 +95,7 @@ open class Electronics : Geometry() {
             return@lazy gdml.structure.assembly {
                 name = "electronicsBox"
                 //physVolume(electronicsBoxVolume, name = "electronicsBox")
-
                 //physVolume(electronicsBoxFillingVolume, name = "electronicsBoxFilling")
-
 
                 physVolume(electronicsCardVolume, name = "electronicsCard1"){
                     position(x = - 15.0 ) { unit = LUnit.MM }
@@ -110,9 +122,141 @@ open class Electronics : Geometry() {
                 physVolume(flatCableVolume, name = "flatCable4"){
                     position(x = 15.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
                 }
+                }
             }
-        }
         
-        return electronicsBoxVolume
+            return electronicsBoxVolume
+            
+        } else {
+            val electronicsBoxVolume: GdmlRef<GdmlAssembly> by lazy {
+
+            //Box 
+            val electronicsBoxOutSolid = gdml.solids.box(
+                electronicsBoxShortSideX,
+                electronicsBoxShortSideY,
+                electronicsBoxLongSideZ,
+                "electronicsBoxOutSolid"
+            )
+
+            val electronicsBoxInSolid = gdml.solids.box(
+                electronicsBoxShortSideX - electronicsBoxThickness*2,
+                electronicsBoxShortSideY - electronicsBoxThickness*2,
+                electronicsBoxLongSideZ - electronicsBoxThickness*2,
+                "electronicsBoxInSolid"
+            )
+
+            val electronicsBoxSolid = gdml.solids.subtraction(
+                electronicsBoxOutSolid, electronicsBoxInSolid,
+                "electronicsBoxSolid")
+
+            //Card
+            val electronicsCardSolid = gdml.solids.box(
+                cardShortSideX,
+                cardShortSideY,
+                cardLongSizeZ,
+                "electronicsCardSolid")
+
+            //Flat cable
+            val flatCableSolid = gdml.solids.box(
+                flatCableShortSideX,
+                flatCableShortSideY,
+                flatCableLongSideZ,
+                "flatCableSolid")
+
+            //chip
+            val chipSolid = gdml.solids.box(
+                chipShortSideX,
+                chipSideYZ,
+                chipSideYZ,
+                "chipSolid")
+
+            //components
+            val componentsSolid = gdml.solids.box(
+                componentsShortSideX,
+                componentsShortSideY,
+                componentsLongSideZ,
+                "componentsSolid")
+
+            val electronicsBoxVolume = 
+            gdml.structure.volume(Materials.Copper.ref,electronicsBoxSolid,"electronicsBoxVolume")
+
+            val electronicsBoxFillingVolume =
+            gdml.structure.volume(Materials.Vacuum.ref,electronicsBoxInSolid,"electronicsBoxFillingVolume")
+
+            val electronicsCardVolume = 
+            gdml.structure.volume(Materials.Copper.ref,electronicsCardSolid,"electronicsCardVolume")
+
+            val flatCableVolume =
+            gdml.structure.volume(Materials.Copper.ref,flatCableSolid,"flatCableVolume")
+
+            val chipVolume =
+            gdml.structure.volume(Materials.Silicon.ref,chipSolid,"chipVolume")
+
+            val componentsVolume =
+            gdml.structure.volume(Materials.Silicon.ref,componentsSolid,"componentsVolume")
+
+            return@lazy gdml.structure.assembly {
+                name = "electronicsBox"
+                //physVolume(electronicsBoxVolume, name = "electronicsBox")
+                //physVolume(electronicsBoxFillingVolume, name = "electronicsBoxFilling")
+
+                physVolume(electronicsCardVolume, name = "electronicsCard1"){
+                    position(x = - 15.0 ) { unit = LUnit.MM }
+                }
+                physVolume(electronicsCardVolume, name = "electronicsCard2"){
+                    position(x = - 5.0 ) { unit = LUnit.MM }
+                }
+                physVolume(electronicsCardVolume, name = "electronicsCard3"){
+                    position(x = 5.0 ) { unit = LUnit.MM }
+                }
+                physVolume(electronicsCardVolume, name = "electronicsCard4"){
+                    position(x = 15.0 ) { unit = LUnit.MM }
+                }
+
+                physVolume(chipVolume, name = "chip1"){
+                    position(x = - 15.0 + cardShortSideX/2 + chipShortSideX/2) { unit = LUnit.MM }
+                }
+                physVolume(chipVolume, name = "chip2"){
+                    position(x = - 5.0 + cardShortSideX/2 + chipShortSideX/2) { unit = LUnit.MM }
+                }
+                physVolume(chipVolume, name = "chip3"){
+                    position(x = 5.0 + cardShortSideX/2 + chipShortSideX/2) { unit = LUnit.MM }
+                }
+                physVolume(chipVolume, name = "chip4"){
+                    position(x = 15.0 + cardShortSideX/2 + chipShortSideX/2) { unit = LUnit.MM }
+                }
+
+                physVolume(componentsVolume, name = "components1"){
+                    position(x = - 15.0 + cardShortSideX/2 + componentsShortSideX/2, z = -30.0 ) { unit = LUnit.MM }
+                }
+                physVolume(componentsVolume, name = "components2"){
+                    position(x = - 5.0 + cardShortSideX/2 + componentsShortSideX/2, z = -30.0 ) { unit = LUnit.MM }
+                }
+                physVolume(componentsVolume, name = "components3"){
+                    position(x = 5.0 + cardShortSideX/2 + componentsShortSideX/2, z = -30.0 ) { unit = LUnit.MM }
+                }
+                physVolume(componentsVolume, name = "components4"){
+                    position(x = 15.0 + cardShortSideX/2 + componentsShortSideX/2, z = -30.0 ) { unit = LUnit.MM }
+                }
+
+                physVolume(flatCableVolume, name = "flatCable1"){
+                    position(x = - 15.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
+                }
+                physVolume(flatCableVolume, name = "flatCable2"){
+                    position(x = - 5.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
+                }
+                physVolume(flatCableVolume, name = "flatCable3"){
+                    position(x = 5.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
+                }
+                physVolume(flatCableVolume, name = "flatCable4"){
+                    position(x = 15.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
+                }
+                }
+            }
+        
+            return electronicsBoxVolume
+
+        }
+
     }
 }
