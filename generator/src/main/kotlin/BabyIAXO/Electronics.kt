3eb73978e4815f41acfd90349b6fdaae6596c478
mariajmz <maria.jzpy@gmail.com>
2025-01-23 18:35:33 +0100

    updating dimensions and materials (now kapton+copper) for BabyIAXO

--- generator/src/main/kotlin/BabyIAXO/Electronics.kt
+++ generator/src/main/kotlin/BabyIAXO/Electronics.kt
@@ -15,26 +15,27 @@ open class Electronics (
         const val electronicsBoxShortSideY: Double = 90.0
         const val electronicsBoxThickness: Double = 5.0
 
-        //Card Dimensions
-        const val cardLongSizeZ: Double = 100.0
-        const val cardShortSideX: Double = 2.5
-        const val cardShortSideY: Double = 70.0
+        //Card (SAFEC) Dimensions
+        const val SAFECLongSideZ: Double = 226.94
+        const val SAFECShortSideX: Double = 0.62
+        const val SAFECShortSideY: Double = 102.6
 
-        //flat cable Dimensions
-        const val flatCableLongSideZ: Double = 150.0
-        const val flatCableShortSideX: Double = 1.0
-        const val flatCableShortSideY: Double = 70.0
+        //flat cable (SAFEC_MM) Dimensions
+        const val SAFEC_MMLongSideZ: Double = 141.0 //had to reduce 20.0 mm to avoid overlap with pipe
+        const val SAFEC_MMShortSideX: Double = 0.2
+        const val SAFEC_MMShortSideY: Double = 102.25
 
         //chip Dimensions
-        const val chipSideYZ: Double = 25.0
+        const val chipSideYZ: Double = 28.0
         const val chipShortSideX: Double = 1.0
 
         //components Dimensions
-        const val componentsLongSideZ: Double = 5.0
-        const val componentsShortSideX: Double = 1.0
-        const val componentsShortSideY: Double = 40.0
+        const val componentsSideZ: Double = 9.3
+        const val componentsSideX: Double = 1.0
+        const val componentsSideY: Double = 76.2
 
         // Distance 
+        const val SAFECLimandeContactDistance: Double = 55.2
         const val DetectorToElectronicsDistanceZ: Double = 235.0
         const val PipeToElectronicsOffSetX: Double = 5.0
         const val PipeToElectronicsDistanceX: Double = 82.0 - (electronicsBoxShortSideX / 2) + PipeToElectronicsOffSetX
@@ -67,18 +68,18 @@ open class Electronics (
                 "electronicsBoxSolid")
 
             //Card
-            val electronicsCardSolid = gdml.solids.box(
-                cardShortSideX,
-                cardShortSideY,
-                cardLongSizeZ,
-                "electronicsCardSolid")
+            val SAFECSolid = gdml.solids.box(
+                SAFECShortSideX/2,
+                SAFECShortSideY,
+                SAFECLongSideZ,
+                "SAFECSolid")
 
             //Flat cable
-            val flatCableSolid = gdml.solids.box(
-                flatCableShortSideX,
-                flatCableShortSideY,
-                flatCableLongSideZ,
-                "flatCableSolid")
+            val SAFEC_MMSolid = gdml.solids.box(
+                SAFEC_MMShortSideX/2,
+                SAFEC_MMShortSideY,
+                SAFEC_MMLongSideZ,
+                "SAFEC_MMSolid")
 
             val electronicsBoxVolume = 
             gdml.structure.volume(Materials.Copper.ref,electronicsBoxSolid,"electronicsBoxVolume")
@@ -86,41 +87,53 @@ open class Electronics (
             val electronicsBoxFillingVolume =
             gdml.structure.volume(Materials.Vacuum.ref,electronicsBoxInSolid,"electronicsBoxFillingVolume")
 
-            val electronicsCardVolume = 
-            gdml.structure.volume(Materials.Copper.ref,electronicsCardSolid,"electronicsCardVolume")
+            val kaptonSAFECVolume = 
+            gdml.structure.volume(Materials.Kapton.ref,SAFECSolid,"kaptonSAFECVolume")
+            val copperSAFECVolume = 
+            gdml.structure.volume(Materials.Copper.ref,SAFECSolid,"copperSAFECVolume")
 
-            val flatCableVolume =
-            gdml.structure.volume(Materials.Copper.ref,flatCableSolid,"flatCableVolume")
+            val copperSAFEC_MMVolume =
+            gdml.structure.volume(Materials.Copper.ref,SAFEC_MMSolid,"copperSAFEC_MMVolume")
+            val kaptonSAFEC_MMVolume =
+            gdml.structure.volume(Materials.Kapton.ref,SAFEC_MMSolid,"kaptonSAFEC_MMVolume")
 
             return@lazy gdml.structure.assembly {
                 name = "electronicsBox"
                 //physVolume(electronicsBoxVolume, name = "electronicsBox")
                 //physVolume(electronicsBoxFillingVolume, name = "electronicsBoxFilling")
 
-                physVolume(electronicsCardVolume, name = "electronicsCard1"){
-                    position(x = - 15.0 ) { unit = LUnit.MM }
-                }
-                physVolume(electronicsCardVolume, name = "electronicsCard2"){
-                    position(x = - 5.0 ) { unit = LUnit.MM }
-                }
-                physVolume(electronicsCardVolume, name = "electronicsCard3"){
-                    position(x = 5.0 ) { unit = LUnit.MM }
-                }
-                physVolume(electronicsCardVolume, name = "electronicsCard4"){
-                    position(x = 15.0 ) { unit = LUnit.MM }
-                }
+                val positionsX = listOf(-15.0, -5.0, 5.0, 15.0)
 
-                physVolume(flatCableVolume, name = "flatCable1"){
-                    position(x = - 15.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
-                }
-                physVolume(flatCableVolume, name = "flatCable2"){
-                    position(x = - 5.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
-                }
-                physVolume(flatCableVolume, name = "flatCable3"){
-                    position(x = 5.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
-                }
-                physVolume(flatCableVolume, name = "flatCable4"){
-                    position(x = 15.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
+                //SAFEC: copperSAFEC{i} + kaptonSAFEC{i}
+                //SAFEC_MM: copperSAFEC_MM{i} + kaptonSAFEC_MM{i}
+                for (i in positionsX.indices) {
+                    val posX = positionsX[i]
+                    val copperName = "copperSAFEC${i + 1}"
+                    val kaptonName = "kaptonSAFEC${i + 1}"
+                    val coppersafecMMName = "copperSAFEC_MM${i + 1}"
+                    val kaptonsafecMMName = "kaptonSAFEC_MM${i + 1}"
+
+                    physVolume(copperSAFECVolume, name = copperName) {
+                        position(x = posX - SAFECShortSideX / 4) { unit = LUnit.MM }
+                    }
+
+                    physVolume(kaptonSAFECVolume, name = kaptonName) {
+                        position(x = posX + SAFECShortSideX / 4) { unit = LUnit.MM }
+                    }
+
+                    physVolume(copperSAFEC_MMVolume, name = coppersafecMMName) {
+                        position(
+                            x = posX + SAFECShortSideX / 2 + SAFEC_MMShortSideX / 4,
+                            z = -SAFECLongSideZ / 2 - SAFEC_MMLongSideZ / 2 + SAFECLimandeContactDistance
+                        ) { unit = LUnit.MM }
+                    }
+
+                    physVolume(kaptonSAFEC_MMVolume, name = kaptonsafecMMName) {
+                        position(
+                            x = posX + SAFECShortSideX / 2 + 3*SAFEC_MMShortSideX / 4,
+                            z = -SAFECLongSideZ / 2 - SAFEC_MMLongSideZ / 2 + SAFECLimandeContactDistance
+                        ) { unit = LUnit.MM }
+                    }
                 }
                 }
             }
@@ -150,18 +163,18 @@ open class Electronics (
                 "electronicsBoxSolid")
 
             //Card
-            val electronicsCardSolid = gdml.solids.box(
-                cardShortSideX,
-                cardShortSideY,
-                cardLongSizeZ,
-                "electronicsCardSolid")
+            val SAFECSolid = gdml.solids.box(
+                SAFECShortSideX/2,
+                SAFECShortSideY,
+                SAFECLongSideZ,
+                "SAFECSolid")
 
             //Flat cable
-            val flatCableSolid = gdml.solids.box(
-                flatCableShortSideX,
-                flatCableShortSideY,
-                flatCableLongSideZ,
-                "flatCableSolid")
+            val SAFEC_MMSolid = gdml.solids.box(
+                SAFEC_MMShortSideX/2,
+                SAFEC_MMShortSideY,
+                SAFEC_MMLongSideZ,
+                "SAFEC_MMSolid")
 
             //chip
             val chipSolid = gdml.solids.box(
@@ -172,9 +185,9 @@ open class Electronics (
 
             //components
             val componentsSolid = gdml.solids.box(
-                componentsShortSideX,
-                componentsShortSideY,
-                componentsLongSideZ,
+                componentsSideX,
+                componentsSideY,
+                componentsSideZ,
                 "componentsSolid")
 
             val electronicsBoxVolume = 
@@ -183,11 +196,15 @@ open class Electronics (
             val electronicsBoxFillingVolume =
             gdml.structure.volume(Materials.Vacuum.ref,electronicsBoxInSolid,"electronicsBoxFillingVolume")
 
-            val electronicsCardVolume = 
-            gdml.structure.volume(Materials.Copper.ref,electronicsCardSolid,"electronicsCardVolume")
+            val kaptonSAFECVolume = 
+            gdml.structure.volume(Materials.Kapton.ref,SAFECSolid,"kaptonSAFECVolume")
+            val copperSAFECVolume = 
+            gdml.structure.volume(Materials.Copper.ref,SAFECSolid,"copperSAFECVolume")
 
-            val flatCableVolume =
-            gdml.structure.volume(Materials.Copper.ref,flatCableSolid,"flatCableVolume")
+            val copperSAFEC_MMVolume =
+            gdml.structure.volume(Materials.Copper.ref,SAFEC_MMSolid,"copperSAFEC_MMVolume")
+            val kaptonSAFEC_MMVolume =
+            gdml.structure.volume(Materials.Kapton.ref,SAFEC_MMSolid,"kaptonSAFEC_MMVolume")
 
             val chipVolume =
             gdml.structure.volume(Materials.Silicon.ref,chipSolid,"chipVolume")
@@ -200,57 +217,48 @@ open class Electronics (
                 //physVolume(electronicsBoxVolume, name = "electronicsBox")
                 //physVolume(electronicsBoxFillingVolume, name = "electronicsBoxFilling")
 
-                physVolume(electronicsCardVolume, name = "electronicsCard1"){
-                    position(x = - 15.0 ) { unit = LUnit.MM }
-                }
-                physVolume(electronicsCardVolume, name = "electronicsCard2"){
-                    position(x = - 5.0 ) { unit = LUnit.MM }
-                }
-                physVolume(electronicsCardVolume, name = "electronicsCard3"){
-                    position(x = 5.0 ) { unit = LUnit.MM }
-                }
-                physVolume(electronicsCardVolume, name = "electronicsCard4"){
-                    position(x = 15.0 ) { unit = LUnit.MM }
+                val positionsX = listOf(-15.0, -5.0, 5.0, 15.0)
+                //SAFEC: copperSAFEC{i} + kaptonSAFEC{i}
+                //SAFEC_MM: copperSAFEC_MM{i} + kaptonSAFEC_MM{i}
+                for (i in positionsX.indices) {
+                    val posX = positionsX[i]
+                    val copperName = "copperSAFEC${i + 1}"
+                    val kaptonName = "kaptonSAFEC${i + 1}"
+                    val coppersafecMMName = "copperSAFEC_MM${i + 1}"
+                    val kaptonsafecMMName = "kaptonSAFEC_MM${i + 1}"
+                    val chipName = "chip${i + 1}"
+                    val componentsName = "components${i + 1}"
+
+                    physVolume(copperSAFECVolume, name = copperName) {
+                        position(x = posX - SAFECShortSideX / 4) { unit = LUnit.MM }
+                    }
+
+                    physVolume(kaptonSAFECVolume, name = kaptonName) {
+                        position(x = posX + SAFECShortSideX / 4) { unit = LUnit.MM }
+                    }
+
+                    physVolume(copperSAFEC_MMVolume, name = coppersafecMMName) {
+                        position(
+                            x = posX + SAFECShortSideX / 2 + SAFEC_MMShortSideX / 4,
+                            z = -SAFECLongSideZ / 2 - SAFEC_MMLongSideZ / 2 + SAFECLimandeContactDistance
+                        ) { unit = LUnit.MM }
+                    }
+
+                    physVolume(kaptonSAFEC_MMVolume, name = kaptonsafecMMName) {
+                        position(
+                            x = posX + SAFECShortSideX / 2 + 3*SAFEC_MMShortSideX / 4,
+                            z = -SAFECLongSideZ / 2 - SAFEC_MMLongSideZ / 2 + SAFECLimandeContactDistance
+                        ) { unit = LUnit.MM }
+                    }
+
+                    physVolume(chipVolume, name = chipName){
+                    position(x = posX + SAFECShortSideX/2 + chipShortSideX/2, z = componentsSideY/2) { unit = LUnit.MM }
+                    }
+                    physVolume(componentsVolume, name = componentsName){
+                    position(x = posX + SAFECShortSideX/2 + componentsSideX/2, z = -componentsSideY/2) { unit = LUnit.MM }
+                    }
                 }
 
-                physVolume(chipVolume, name = "chip1"){
-                    position(x = - 15.0 + cardShortSideX/2 + chipShortSideX/2) { unit = LUnit.MM }
-                }
-                physVolume(chipVolume, name = "chip2"){
-                    position(x = - 5.0 + cardShortSideX/2 + chipShortSideX/2) { unit = LUnit.MM }
-                }
-                physVolume(chipVolume, name = "chip3"){
-                    position(x = 5.0 + cardShortSideX/2 + chipShortSideX/2) { unit = LUnit.MM }
-                }
-                physVolume(chipVolume, name = "chip4"){
-                    position(x = 15.0 + cardShortSideX/2 + chipShortSideX/2) { unit = LUnit.MM }
-                }
-
-                physVolume(componentsVolume, name = "components1"){
-                    position(x = - 15.0 + cardShortSideX/2 + componentsShortSideX/2, z = -30.0 ) { unit = LUnit.MM }
-                }
-                physVolume(componentsVolume, name = "components2"){
-                    position(x = - 5.0 + cardShortSideX/2 + componentsShortSideX/2, z = -30.0 ) { unit = LUnit.MM }
-                }
-                physVolume(componentsVolume, name = "components3"){
-                    position(x = 5.0 + cardShortSideX/2 + componentsShortSideX/2, z = -30.0 ) { unit = LUnit.MM }
-                }
-                physVolume(componentsVolume, name = "components4"){
-                    position(x = 15.0 + cardShortSideX/2 + componentsShortSideX/2, z = -30.0 ) { unit = LUnit.MM }
-                }
-
-                physVolume(flatCableVolume, name = "flatCable1"){
-                    position(x = - 15.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
-                }
-                physVolume(flatCableVolume, name = "flatCable2"){
-                    position(x = - 5.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
-                }
-                physVolume(flatCableVolume, name = "flatCable3"){
-                    position(x = 5.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
-                }
-                physVolume(flatCableVolume, name = "flatCable4"){
-                    position(x = 15.0, z = - cardLongSizeZ / 2 - flatCableLongSideZ / 2) { unit = LUnit.MM }
-                }
                 }
             }
         
